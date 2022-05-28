7899299acd107168bad30687d7016b8e9719abb5
wifekatt <wifek.attia.2000@gmail.com>
2022-05-28 23:26:16 +0100

    first commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRippleModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
+
+
+import { CandidatComponent } from '../../candidat/candidat/candidat.component';
+import { CandidatListComponent } from '../../candidat-list/candidat-list/candidat-list.component';
+import { UrnesComponent } from '../../urnes/urnes.component';
+import { VoteComponent } from '../../vote/vote/vote.component';
+import { ElecteurComponent } from '../../electeur/electeur/electeur.component';
+import { AdminComponent } from '../../admin/admin/admin.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    AdminComponent,
+    CandidatComponent,
+    CandidatListComponent,
+    ElecteurComponent,
+    UrnesComponent,
+    VoteComponent,
+
+  ]
+})
+
+export class AdminLayoutModule {}
