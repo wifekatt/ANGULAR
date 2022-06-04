92cbff86543b9d5fb121a59a668c3abba52634ee
wifekatt <wifek.attia.2000@gmail.com>
2022-06-05 00:44:19 +0100

    images

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full',
+  },
   {path:"login", component: LoginComponent},
   {path:"home", component: HomeComponent},
 ];
