d99a226ecb0857f57d4e39650b8d2f1e06782f45
wifekatt <wifek.attia.2000@gmail.com>
2022-06-08 20:21:30 +0100

    login user

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { Electeur } from '../class/electeur';
+import { AuthService } from '../service/AuthService';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,11 +9,30 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email!: Electeur["email"];
+  CIN!: Electeur["CIN"];
+errorMessage = 'Invalid Credentials';
+successMessage!: string;
+invalidLogin = false;
+loginSuccess = false;
+
+  constructor(private authService: AuthService) { }
 
 
   ngOnInit(): void {
   }
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   CINFormControl = new FormControl('', [Validators.required]);
+
+  handleLogin() {
+    this.authService.login(this.CIN, this.email).subscribe((result) => {
+      this.invalidLogin = false;
+      this.loginSuccess = true;
+      this.successMessage = 'Login Successful';
+      // redirect to main page
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });
+  }
 }
